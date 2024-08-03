f9df7ab94d1a797e1c438e28a620ddc6f8c5f0dd
NaomiWambuiN <mburujoseph534@gmail.com>
2024-08-03 10:36:10 +0300

    first commit

---
+++ NEW/new.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+  var navbarLinks = document.getElementById("navbar-links");
+  if (navbarLinks.style.display === "block") {
+    navbarLinks.style.display = "none";
+  } else {
+    navbarLinks.style.display = "block";
+  }
+}
